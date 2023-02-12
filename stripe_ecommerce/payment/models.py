9b350713f3b1d4acd19ef3dd2a56bd1ed91ae743
alex <shashin01@mail.ru>
2023-02-12 03:09:57 +0300

    create app - payment

---
+++ stripe_ecommerce/payment/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+
+
+class Item(models.Model):
+    name = models.CharField(max_length=255)
+    description = models.TextField()
+    price = models.FloatField()
+
+    def __str__(self):
+        return self.name
